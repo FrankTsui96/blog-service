499c3dc298b849e760dfce8ab8c21c1731e9fd1c
franktsui96 <cuix119@gmail.com>
2026-02-14 21:51:44 +0800

    feat: 清理测试用 service 和 controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello() {
-    return {
-      status: 'success',
-      message: 'Hello from NestJS!!!!!',
-      timestamp: new Date().toISOString(),
-      data: null,
-    };
-  }
-}
+export class AppService {}
